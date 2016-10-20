e1de572df0ef69a2dc1796434fdc7c8ae776d7a4
PieraMarchesini <pieracarolina@yahoo.com.br>
2016-10-20 00:59:11 -0200

    Inclusão da classe Main

---
+++ Sistema de Chamados/src/SistemaChamadosMain.java
@@ -0,0 +1,21 @@
+
+import com.br.es2.controller.ControladorPrincipal;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Piera
+ */
+public class SistemaChamadosMain {
+	
+	public static void main(String[] args){
+		
+		new ControladorPrincipal().start();
+	}
+	
+}
