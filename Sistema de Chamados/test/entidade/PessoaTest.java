8da796e69409f3e38cc7387077e066054231f733
palomaaraujo <paloma.araujo6@hotmail.com>
2016-11-03 00:06:13 -0200

    Update PessoaTest.java

--- Sistema de Chamados/test/entidade/PessoaTest.java
+++ Sistema de Chamados/test/entidade/PessoaTest.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidade;
 
-import entidade.Pessoa;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,69 +8,41 @@ import static org.junit.Assert.*;
  * @author 31581773
  */
 public class PessoaTest {
-    Pessoa p = new Pessoa("Mariana", 30817564);
+    
+    Pessoa p;
+    
     public PessoaTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getNome method, of class Pessoa.
-     */
     @Test
-    public void testGetNome() {
+    public void criarPessoaTest(){
+        p = new Pessoa("Mariana", 30817564);
+        
         assertEquals("Mariana", p.getNome());
+        assertEquals(30817564, p.getTelefone());
     }
-
-    /**
-     * Test of setNome method, of class Pessoa.
-     */
+    
     @Test
-    public void testSetNome() {
+    public void editarPessoaTest(){
+        p = new Pessoa("Mariana", 30817564);
         p.setNome("Paloma");
+        p.setTelefone(30224761);
+        
         assertEquals("Paloma", p.getNome());
+        assertEquals(30224761, p.getTelefone());
     }
+ 
     @Test 
     public void NomeInvalidoPassa(){
+        p = new Pessoa("Mariana", 30817564);
         p.setNome("Mari@ M@d@l$en@");
         assertEquals("Mari@ M@d@l$en@", p.getNome());
     }
 
-    /**
-     * Test of getTelefone method, of class Pessoa.
-     */
-    @Test
-    public void testGetTelefone() {
-        assertEquals(30817564,p.getTelefone());
-    }
-
-    /**
-     * Test of setTelefone method, of class Pessoa.
-     */
-    @Test
-    public void testSetTelefone() {
-        p.setTelefone(972616068);
-        assertEquals(972616068,p.getTelefone());
-    }
-    
     @Test
     public void TelefoneInvalidoPassa(){
+        p = new Pessoa("Mariana", 30817564);
         p.setTelefone(123);
         assertEquals(123,p.getTelefone());
     }
-    
 }
