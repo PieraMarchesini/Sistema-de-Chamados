b9287785cf5919ecac27074b8b6905e05a17700d
palomaaraujo <paloma.araujo6@hotmail.com>
2016-11-03 00:05:34 -0200

    Update EmpresaTest.java

--- Sistema de Chamados/test/entidade/EmpresaTest.java
+++ Sistema de Chamados/test/entidade/EmpresaTest.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidade;
 
-import entidade.Empresa;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,67 +8,28 @@ import static org.junit.Assert.*;
  * @author 31581773
  */
 public class EmpresaTest {
-    Empresa emp = new Empresa(123456,"Duratex");
+    
+    Empresa emp;
     
     public EmpresaTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+    @Test
+    public void criarEmpresaTest(){
+        emp = new Empresa(123456,"Duratex");
+        
+        assertEquals(123456, emp.getNumeroContrato());
+        assertEquals("Duratex", emp.getNomeEmpresa());
     }
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getNumeroContrato method, of class Empresa.
-     */
     @Test
-    public void testGetNumeroContrato() {
-        assertEquals(123456,emp.getNumeroContrato());
-    }
-
-    /**
-     * Test of setNumeroContrato method, of class Empresa.
-     */
-    @Test
-    public void testSetNumeroContrato() {
-        emp.setNumeroContrato(123465);
-        assertEquals(123465,emp.getNumeroContrato());
-    }
-
-    /**
-     * Test of getNomeEmpresa method, of class Empresa.
-     */
-    @Test
-    public void testGetNomeEmpresa() {
-        assertEquals("Duratex",emp.getNomeEmpresa());
-    }
-
-    /**
-     * Test of setNomeEmpresa method, of class Empresa.
-     */
-    @Test
-    public void testSetNomeEmpresa() {
+    public void editarEmpresaTest(){
+        emp = new Empresa(123456,"Duratex");
         emp.setNomeEmpresa("Twitter");
-        assertEquals("Twitter",emp.getNomeEmpresa());
+        emp.setNumeroContrato(7891011);
+        
+        assertEquals("Twitter", emp.getNomeEmpresa());
+        assertEquals(7891011, emp.getNumeroContrato());
     }
-
-    /**
-     * Test of toString method, of class Empresa.
-     */
-    @Test
-    public void testToString() {
-        assertEquals("123456 - Duratex",emp.toString());
-    }
-    
 }
+
