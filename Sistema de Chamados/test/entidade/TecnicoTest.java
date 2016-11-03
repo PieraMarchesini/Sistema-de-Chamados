0eeb93db1de506a81eb466502ee9b2e81704024b
palomaaraujo <paloma.araujo6@hotmail.com>
2016-11-03 00:07:56 -0200

    Update TecnicoTest.java

--- Sistema de Chamados/test/entidade/TecnicoTest.java
+++ Sistema de Chamados/test/entidade/TecnicoTest.java
@@ -1,65 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidade;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
+ 
+ /**
+  *
+  * @author 31581773
+  */
+ public class TecnicoTest {
 
-/**
- *
- * @author 31581773
- */
-public class TecnicoTest {
-    
+     Tecnico tec;
+     
     public TecnicoTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
-    public void inserirTecnico(){
-        Tecnico tec = new Tecnico("Mariana", 972616068);
-        Assert.assertEquals("Mariana", tec.getNome());
-        Assert.assertEquals(972616068, tec.getTelefone());
-    }
-    @Test
-    public void inserirTelefoneInvalido(){
-        Tecnico tec = new Tecnico("Paloma", 97261);
-       Assert.assertEquals(97261, tec.getTelefone());
-    }
-    @Test
-    public void inserirNomeCaracteres(){
-       Tecnico tec = new Tecnico("P!er@", 972616068);
-       Assert.assertEquals("P!er@",tec.getNome());
-    }
-    /**
-     * Test of toString method, of class Tecnico.
-     */
-    
-    @Test
-    public void testToString() {
+    public void inserirTecnicoTest(){
+        tec = new Tecnico("Mariana", 972616068);
+        assertEquals("Mariana", tec.getNome());
+        assertEquals(972616068, tec.getTelefone());
     }
     
+    public void editarTecnicoTest(){
+        tec = new Tecnico("Mariana", 972616068);
+        tec.setNome("Mariana Macedo");
+        tec.setTelefone(97261606);
+        
+        assertEquals("Mariana Macedo", tec.getNome());
+        assertEquals(97261606, tec.getTelefone());
+    }
+     
+    @Test
+    public void inserirTelefoneInvalidoTest(){
+        tec = new Tecnico("Paloma", 97261);
+        assertEquals(97261, tec.getTelefone());
+    }
+      
+    @Test
+    public void inserirNomeCaracteresTest(){
+        tec = new Tecnico("P!er@", 972616068);
+        assertEquals("P!er@",tec.getNome());
+    } 
 }
+
