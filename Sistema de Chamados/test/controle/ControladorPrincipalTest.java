5efaa6493fa8a4311e5f835ce1c89d4e34dbe185
Mariana <Mariana@Mariana-VAIO>
2016-11-02 23:58:25 -0200

    Criação de Cenários ControleChamados e ControlePrincipal

--- Sistema de Chamados/test/controle/ControladorPrincipalTest.java
+++ Sistema de Chamados/test/controle/ControladorPrincipalTest.java
@@ -5,6 +5,17 @@
  */
 package controle;
 
+import entidade.Chamado;
+import entidade.ClienteEmpresa;
+import entidade.Empresa;
+import entidade.RegistroChamado;
+import entidade.Tecnico;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,87 +28,131 @@ import static org.junit.Assert.*;
  * @author 31581773
  */
 public class ControladorPrincipalTest {
-    
+
+    private ControladorPrincipal controladorPrincipal;
+
     public ControladorPrincipalTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException, IOException {
+        FileOutputStream fos = new FileOutputStream("tecnicos.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        HashMap<Integer, Tecnico> cacheTecnico = new HashMap<>();
+
+        cacheTecnico.put(1, new Tecnico("Marineide", 38716465));
+
+        oos.writeObject(cacheTecnico);
+
+        oos.flush();
+        fos.flush();
+
+        oos.close();
+        fos.close();
+
+        fos = new FileOutputStream("empresas.dat");
+        oos = new ObjectOutputStream(fos);
+
+        HashMap<Long, Empresa> cacheEmpresa = new HashMap<>();
+
+        cacheEmpresa.put((long) 1234, new Empresa(1234, "Apple"));
+
+        oos.writeObject(cacheEmpresa);
+
+        oos.flush();
+        fos.flush();
+
+        oos.close();
+        fos.close();
+
+        fos = new FileOutputStream("clientes.dat");
+        oos = new ObjectOutputStream(fos);
+
+        HashMap<Long, ClienteEmpresa> cacheCliente = new HashMap<>();
+
+        String cpf = "45661789397";
+
+        cacheCliente.put(Long.parseLong(cpf), new ClienteEmpresa(1, new Empresa(1234, "Apple"), Long.parseLong(cpf), "Marineide", 38716465));
+
+        oos.writeObject(cacheCliente);
+
+        oos.flush();
+        fos.flush();
+
+        oos.close();
+        fos.close();
+
+        fos = new FileOutputStream("chamados.dat");
+        oos = new ObjectOutputStream(fos);
+
+        HashMap<Integer, Chamado> cacheChamado = new HashMap<>();
+        Tecnico tec1 = new Tecnico("Mariana", 31546463);
+        ClienteEmpresa ce = new ClienteEmpresa(1, new Empresa(1010, "Samsung"), 123456789, "Amanda", 987456152);
+        cacheChamado.put(1, new Chamado("banco de dados", "banco de dados não conecta", 1, tec1, ce, "Windows", "7", "BD1"));
+
+        oos.writeObject(cacheChamado);
+
+        oos.flush();
+        fos.flush();
+
+        oos.close();
+        fos.close();
+
+        fos = new FileOutputStream("registroChamados.dat");
+        oos = new ObjectOutputStream(fos);
+
+        HashMap<Integer, RegistroChamado> cacheRegistro = new HashMap<>();
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+
+        String cpf1 = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce1 = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+
+        cacheRegistro.put(1, new RegistroChamado("Teste", new Chamado(ce1.getCodigo(), "Chamado de Rede", "Mapeamento de pasta", 3, tec, ce1, "Windows", "7", "cabo de rede", "172.21.3.35"), tec));
+
+        oos.writeObject(cacheRegistro);
+
+        oos.flush();
+        fos.flush();
+
+        oos.close();
+        fos.close();
     }
-    
+
     @After
     public void tearDown() {
+        File fileEmp = new File("tecnicos.dat");
+        fileEmp.delete();
+        File fileTec = new File("empresas.dat");
+        fileTec.delete();
+        File fileCli = new File("clientes.dat");
+        fileCli.delete();
+        File fileCha = new File("chamados.dat");
+        fileCha.delete();
+        File fileReg = new File("registroChamados.dat");
+        fileReg.delete();
     }
 
     /**
      * Test of start method, of class ControladorPrincipal.
      */
     @Test
-    public void testStart() {
-        }
+    public void testArquivosCriados() {
+        controladorPrincipal = new ControladorPrincipal();
+        assertNotNull(controladorPrincipal.getCtrTecnicos());
+        assertNotNull(controladorPrincipal.getCtrEmpresa());
+        assertNotNull(controladorPrincipal.getCtrChamados());
+        assertNotNull(controladorPrincipal.getCtrClientes());
 
-    /**
-     * Test of getCtrEmpresa method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testGetCtrEmpresa() {
-       }
+    }
 
-    /**
-     * Test of setCtrEmpresa method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testSetCtrEmpresa() {
-       }
-
-    /**
-     * Test of getCtrChamados method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testGetCtrChamados() {
-       }
-
-    /**
-     * Test of setCtrChamados method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testSetCtrChamados() {
-       }
-
-    /**
-     * Test of getCtrClientes method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testGetCtrClientes() {
-       }
-
-    /**
-     * Test of setCtrClientes method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testSetCtrClientes() {
-       }
-
-    /**
-     * Test of getCtrTecnicos method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testGetCtrTecnicos() {
-       }
-
-    /**
-     * Test of setCtrTecnicos method, of class ControladorPrincipal.
-     */
-    @Test
-    public void testSetCtrTecnicos() {
-       }
-    
 }
--- Sistema de Chamados/test/controle/ControleChamadosTest.java
+++ Sistema de Chamados/test/controle/ControleChamadosTest.java
@@ -7,10 +7,17 @@ package controle;
 
 import entidade.Chamado;
 import entidade.ClienteEmpresa;
+import entidade.Empresa;
 import entidade.RegistroChamado;
 import entidade.Tecnico;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,143 +28,150 @@ import static org.junit.Assert.*;
  * @author 31581773
  */
 public class ControleChamadosTest {
-    
+
+    private ControleChamados controleChamados;
+    private final int tempoTimeOut = 800;
+
     public ControleChamadosTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("chamados.dat")) {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            HashMap<Integer, Chamado> cacheChamados = new HashMap<>();
+            Tecnico tec = new Tecnico("Mariana", 31546463);
+            String cpf = "11568375379";
+            Empresa emp = new Empresa(123456, "Apple");
+            ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+
+            cacheChamados.put(2, new Chamado("banco de dados", "banco de dados não conecta", 1, tec, ce, "Windows", "7", "BD1"));
+            oos.writeObject(cacheChamados);
+
+            oos.flush();
+            fos.flush();
+
+            oos.close();
+        }
     }
-    
+
     @After
     public void tearDown() {
+        File file = new File("chamados.dat");
+        file.delete();
     }
 
-    /**
-     * Test of alterarChamado method, of class ControleChamados.
-     */
-    @Test
-    public void testAlterarChamado_4args() {
-    }
-
-    /**
-     * Test of InserirChamadoRede method, of class ControleChamados.
-     */
+    // teste para inserir chamado de rede
     @Test
     public void testInserirChamadoRede() {
+        controleChamados = new ControleChamados();
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+        String cpf = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+
+        Chamado chamado = controleChamados.InserirChamadoRede("Rede", "Sem mapeamento das pastas e rede",
+                2, tec, ce, "Windows", "7", "cabo de rede", "172.21.3.35");
+        chamado.setEnderecoRede("172.21.3.35");
+        chamado.setSistemaOperacional("Windows");
+        assertEquals("Rede", chamado.getTitulo());
+        assertEquals("Sem mapeamento das pastas e rede", chamado.getDescricao());
+        assertEquals(2, chamado.getPrioridade());
+        assertEquals(tec, chamado.getTecnico());
+        assertEquals(ce, chamado.getCliente());
+        assertEquals("Windows", chamado.getSistemaOperacional());
+        assertEquals("7", chamado.getVersaoSO());
+        assertEquals("cabo de rede", chamado.getTipoConexao());
+        assertEquals("172.21.3.35", chamado.getEnderecoRede());
     }
 
     /**
      * Test of InserirChamadoBancoDeDados method, of class ControleChamados.
      */
-    @Test
+    //teste para inserir chamado de bd
+    @Test(timeout = tempoTimeOut)
     public void testInserirChamadoBancoDeDados() {
+        controleChamados = new ControleChamados();
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+        String cpf = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+        Chamado chamado = controleChamados.InserirChamadoBancoDeDados("Banco de Dados", "insercao de novas colunas",
+                4, tec, ce, "Windows", "7", "SQL");
+        assertEquals("Banco de Dados", chamado.getTitulo());
+        assertEquals("insercao de novas colunas", chamado.getDescricao());
+        assertEquals(4, chamado.getPrioridade());
+        assertEquals(tec, chamado.getTecnico());
+        assertEquals(ce, chamado.getCliente());
+        assertEquals("Windows", chamado.getSistemaOperacional());
+        assertEquals("7", chamado.getVersaoSO());
+        assertEquals("SQL", chamado.getBancoDeDados());
     }
 
     /**
      * Test of InserirChamadoDesempenho method, of class ControleChamados.
      */
-    @Test
+    @Test(timeout = tempoTimeOut)
     public void testInserirChamadoDesempenho() {
+        controleChamados = new ControleChamados();
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+        String cpf = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+
+        Chamado chamado = controleChamados.InserirChamadoDesempenho("Máquina lenta", "Todos os programas travam e demora 15 minutos para a máquina iniciar",
+                4, tec, ce, "Windows", "7", "verificar disco", 20.10);
+        assertEquals("Máquina lenta", chamado.getTitulo());
+        assertEquals("Todos os programas travam e demora 15 minutos para a máquina iniciar", chamado.getDescricao());
+        assertEquals(4, chamado.getPrioridade());
+        assertEquals(tec, chamado.getTecnico());
+        assertEquals(ce, chamado.getCliente());
+        assertEquals("Windows", chamado.getSistemaOperacional());
+        assertEquals("7", chamado.getVersaoSO());
+        assertEquals("verificar disco", chamado.getOperacao());
+        assertEquals(20.10, chamado.getDuracaoOperacao(), 0.0);
     }
 
-    /**
-     * Test of inserirRegistroChamado method, of class ControleChamados.
-     */
-    @Test
-    public void testInserirRegistroChamado() {
+   
+    //Definir Status
+    @Test (timeout = tempoTimeOut)
+    public void testAlterarChamado() {
+        controleChamados = new ControleChamados();
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+        String cpf = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+
+        Chamado chamado = new Chamado("banco de dados", "banco de dados não conecta", 1, tec, ce, "Windows", "7", "BD1");
+        ce.setCodigo(100);
+        chamado.setStatus("Pendente");
+  
+        assertEquals("Pendente", chamado.getStatus());
+      
     }
 
-    /**
-     * Test of cadastrarChamado method, of class ControleChamados.
-     */
-    @Test
-    public void testCadastrarChamado() {
-    }
-
-    /**
-     * Test of fecharTela method, of class ControleChamados.
-     */
-    @Test
-    public void testFecharTela() {
-    }
-
-    /**
-     * Test of voltaChamadoCodigo method, of class ControleChamados.
-     */
-    @Test
-    public void testVoltaChamadoCodigo() {
-    }
-
-    /**
-     * Test of setChamadoAlterado method, of class ControleChamados.
-     */
-    @Test
-    public void testSetChamadoAlterado() {
-    }
-
-    /**
-     * Test of getChamadoAlterado method, of class ControleChamados.
-     */
-    @Test
-    public void testGetChamadoAlterado() {
-    }
-
-    /**
-     * Test of emitirRelatorios method, of class ControleChamados.
-     */
-    @Test
-    public void testEmitirRelatorios() {
-    }
-
-    /**
-     * Test of buscaPeloCodigo method, of class ControleChamados.
-     */
-    @Test
-    public void testBuscaPeloCodigo() {
-    }
-
-    /**
-     * Test of retornaDetalhesChamado method, of class ControleChamados.
-     */
-    @Test
-    public void testRetornaDetalhesChamado() {
-    }
-
-    /**
-     * Test of alterarChamado method, of class ControleChamados.
-     */
-    @Test
-    public void testAlterarChamado_0args() {
-    }
-
-    /**
-     * Test of abrirRelatorio method, of class ControleChamados.
-     */
-    @Test
-    public void testAbrirRelatorio() {
-    }
-
-    /**
-     * Test of fecharTelaAlteracao method, of class ControleChamados.
-     */
-    @Test
-    public void testFecharTelaAlteracao() {
-    }
-
-    /**
-     * Test of validarQtdChamados method, of class ControleChamados.
-     */
-    @Test
+    @Test (timeout = tempoTimeOut)
     public void testValidarQtdChamados() {
+        controleChamados = new ControleChamados();
+
+        Tecnico tec = new Tecnico("Mariana", 31546463);
+        String cpf = "11568375379";
+        Empresa emp = new Empresa(123456, "Apple");
+        ClienteEmpresa ce = new ClienteEmpresa(12, emp, Long.parseLong(cpf), "Gisleine", 30817564);
+        Chamado chamado = new Chamado("banco de dados", "banco de dados não conecta", 1, tec, ce, "Windows", "7", "BD1");
+
+        int qtd = controleChamados.validarQtdChamados(ce);
+
+        Assert.assertEquals(1, qtd);
     }
-    
+
 }
